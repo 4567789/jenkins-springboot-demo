836c8a05b423cb14154586dc5fadf17734730eb8
BussDev <medounebus@gmail.com>
2026-08-30 20:33:44 +0000

    Initial Spring Boot project

---
+++ src/test/java/org/example/jenkinsspringboot/JenkinsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jenkinsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
